c66b43789daf555c28c64a59f31fb84fefc9f53d
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2020-08-06 00:36:32 +0800

    add database

--- reuters_crawler/pipelines.py
+++ reuters_crawler/pipelines.py
@@ -4,8 +4,22 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import sqlite3
 
 
 class ReutersCrawlerPipeline(object):
+	def open_spider(self, spider):
+		self.con = sqlite3.connect("reuters.db")
+		self.cur = self.con.cursor()
+
     def process_item(self, item, spider):
+    	insert_query = \
+    		"""
+    		INSERT INTO reuters (title, text, date)
+    		VALUES('{}', '{}', '{}')
+    		""".format(item['title'], item['text'], item['date'])
+    	self.cur.execute(insert_query)
         return item
+
+    def spider_close(self, spider):
+    	self.con.close()
